dd68c6896f2489edb04281f1e6290cb3458771fe
minnahei <minna.s.heinonen@gmail.com>
2020-03-10 11:23:54 +0100

    Added classes

---
+++ CSProject/FileReader.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using static System.Console;
+namespace CSProject
+
+{
+    public class FileReader
+    {
+
+        public List<Staff> ReadFile()
+        {
+            List<Staff> myStaff = new List<Staff>();
+            string[] result = new string[2];
+            string path = "staff.txt";
+            string[] separator = { ", " };
+
+            if (File.Exists(path))
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    while (sr.EndOfStream == false)
+                    {
+                        //Read a line from the file, storing the name in the first array element,
+                        //and the position in the second element.
+                        result = sr.ReadLine().Split(separator, StringSplitOptions.RemoveEmptyEntries);
+
+                        //Check the position, and add the appropriate Staff object to the list.
+                        if (result[1] == "Manager")
+                            myStaff.Add(new Manager(result[0]));
+                        else if (result[1] == "Admin")
+                            myStaff.Add(new Admin(result[0]));
+                        else
+                            WriteLine("Invalid position: {0}", result[1]);
+                    }
+
+                    sr.Close();
+                }
+            }
+            else
+                WriteLine("File not found: {0}", path);
+
+            return myStaff;
+        }
+    }
+}
--- CSProject/PaySlip.cs
+++ CSProject/PaySlip.cs
@@ -1,30 +1,100 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using static System.Console;
+
 namespace CSProject
 {
-    public class PaySlip
+    class PaySlip
     {
-        private int month;
-        private int year;
+        //Fields
+        private int month, year;
 
-        enum MonthsofYear
+        //Enum (private by default inside a class)
+        enum MonthsOfYear { JAN = 1, FEB = 2, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC }
+
+        //Constructor
+        public PaySlip(int payMonth, int payYear)
         {
-            JAN = 1, FEB = 2, MAR = 3, APR = 4, MAY = 5, JUN = 6, JUL = 7, AUG = 8, SEP = 9, OCT = 10, NOV = 11, DEC = 12
+            month = payMonth;
+            year = payYear;
         }
 
-        public PaySlip (int Month, int Year)
-        {
-            month = Month;
-            year = Year;
-        }
-
-        public void GeneratePaySlip(List<Staff>myStaff)
+        //Methods
+        public void GeneratePaySlip(List<Staff> myStaff)
         {
             string path;
 
+            WriteLine("Generating pay slips...");
+
             foreach (Staff f in myStaff)
             {
+                path = f.NameOfStaff + ".txt";
 
+                using (StreamWriter sw = new StreamWriter(path, true))
+                {
+                    //Print the element # "month" from the MonthsOfYear enum
+                    sw.WriteLine("PAY SLIP FOR {0} {1}", (MonthsOfYear)month, year);
+
+                    //Generate a string by repeating the '=' char 25 times.
+                    sw.WriteLine(new String('=', 25));
+                    sw.WriteLine("Name of Staff: {0}", f.NameOfStaff);
+                    sw.WriteLine("Hours Worked: {0}", f.HoursWorked);
+                    sw.WriteLine("");
+
+                    //Use [C]urrency format for the BasicPay amount.
+                    sw.WriteLine("Basic Pay: {0:C}", f.BasicPay);
+
+                    //Depending on the typeof Staff class being processed, cast f into an
+                    //object of that type, so we can access the properties of that class.
+                    if (f.GetType() == typeof(Manager))
+                        sw.WriteLine("Allowance: {0:C}", ((Manager)f).Allowance);
+                    else if (f.GetType() == typeof(Admin))
+                        sw.WriteLine("Overtime Pay: {0:C}", ((Admin)f).Overtime);
+
+                    sw.WriteLine("");
+                    sw.WriteLine(new String('=', 25));
+                    sw.WriteLine("Total Pay: {0:C}", f.TotalPay);
+                    sw.WriteLine(new String('=', 25));
+
+                    sw.Close();
+                }
             }
+        }
+
+        public void GenerateSummary(List<Staff> myStaff)
+        {
+            string path = "summary.txt";
+
+            WriteLine("Generating summary...");
+
+            //Retrieve a list of staff with less than 10 hours.
+            var result =
+                from st in myStaff
+                where st.HoursWorked < 10
+                orderby st.NameOfStaff
+                select new { st.NameOfStaff, st.HoursWorked };
+
+            using (StreamWriter sw = new StreamWriter(path, true))
+            {
+                sw.WriteLine("Staff with less than 10 working hours for {0} {1}", (MonthsOfYear)month, year);
+                sw.WriteLine("");
+
+                foreach (var f in result)
+                {
+                    sw.WriteLine("Name of Staff: {0}, Hours Worked: {1}", f.NameOfStaff, f.HoursWorked);
+                }
+                sw.WriteLine(new String('=', 25));
+
+                sw.Close();
+            }
+        }
+
+        public override string ToString()
+        {
+            return "\nMonth = " + (MonthsOfYear)month
+                + "\nYear = " + year;
         }
     }
 }
